4a3fdfd9921280e363d3ad053370483919586651
Hasnaa Ayman <hassnaaayman16@gmail.com>
2021-12-16 19:13:04 +0200

    first commit

---
+++ precache-manifest.358d8f466ace8a0e1adca4556a358704.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e59f6e9f516022d2fcba42de2b9377",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "405d77bed7687bfdaf83",
+    "url": "/Portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "4f86de8502822817a581",
+    "url": "/Portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "8e5b78a96fe3bb672063",
+    "url": "/Portfolio/static/css/main.60ceaa30.chunk.css"
+  },
+  {
+    "revision": "67606e66e8c67b00e34a",
+    "url": "/Portfolio/static/js/2.c523b2f9.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/Portfolio/static/js/2.c523b2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405d77bed7687bfdaf83",
+    "url": "/Portfolio/static/js/3.cf9c83e3.chunk.js"
+  },
+  {
+    "revision": "4f86de8502822817a581",
+    "url": "/Portfolio/static/js/4.651aed64.chunk.js"
+  },
+  {
+    "revision": "8e5b78a96fe3bb672063",
+    "url": "/Portfolio/static/js/main.4f58737c.chunk.js"
+  },
+  {
+    "revision": "8d471c4382e46f74f723",
+    "url": "/Portfolio/static/js/runtime-main.674774de.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "4ec7cd7a6f0ba5718783ba6475efbf57",
+    "url": "/Portfolio/static/media/facultyOfScience.4ec7cd7a.jpeg"
+  },
+  {
+    "revision": "1e94adf7ebaa43cf77f962a1cbf709e5",
+    "url": "/Portfolio/static/media/iti_logo.1e94adf7.jpeg"
+  },
+  {
+    "revision": "0ae73ca53a784311c42a74dd14822bc8",
+    "url": "/Portfolio/static/media/logo.0ae73ca5.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
